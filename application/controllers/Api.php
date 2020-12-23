ea65324903ee512872f606bd92c7e43108f2a8f8
Nosakhare Atekha Endurance <nosakharee.16@gmail.com>
2020-12-22 23:15:15 -0800

    performing http post request from the mobile app

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -24,8 +24,13 @@ class Api extends My_Controller {
                 $author_img =$row['author_image'];
                 $time       =$row['time'];
                 $type       =$row['type'];
+                $is_background  =$row['is_background'];
+                $background_link    =$row['background_link'];
+                $cat_id         =$row['cat_id'];
 
-                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully');
+                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully',
+                'is_background'=>$is_background,'background_link'=>$background_link,
+                'cat_id'=>$cat_id);
             }
         }else{
             $msg[]  =array('status'=>'fail','status_msg'=>'Couldn\'t fetch data from server');
@@ -46,8 +51,13 @@ class Api extends My_Controller {
                 $author_img =$row['author_image'];
                 $time       =$row['time'];
                 $type       =$row['type'];
+                $is_background  =$row['is_background'];
+                $background_link    =$row['background_link'];
+                $cat_id         =$row['cat_id'];
 
-                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully');
+                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully',
+                'is_background'=>$is_background,'background_link'=>$background_link,
+                'cat_id'=>$cat_id);
             }
         }else{
             $msg[]  =array('status'=>'fail','status_msg'=>'Couldn\'t fetch data from server, reason be that Quote ID not found or Server is Busy!');
@@ -55,6 +65,51 @@ class Api extends My_Controller {
         echo json_encode($msg);
 	}
 
+
+    public function get_category(){
+        $msg    =array();
+		$action   =$this->Action->get_category();
+        if(is_array($action)){
+            foreach($action as $row){
+                $cat_id         =$row['cat_id'];
+                $cat_name       =$row['cat_name'];
+
+
+                $msg[]      =array('cat_id'=>$cat_id,'cat_name'=>$cat_name);
+            }
+        }else{
+            $msg[]  =array('status'=>'fail','status_msg'=>'Couldn\'t fetch data from server');
+        }
+        echo json_encode($msg);
+	}
+
+
+	public function get_quote_by_cat_id($id){
+        $msg    =array();
+		$action   =$this->Action->get_quote_by_cat_id($id);
+        if(is_array($action)){
+            foreach($action as $row){
+                $id         =$row['id'];
+                $title      =$row['title'];
+                $desc       =$row['body'];
+                $image      =$row['image'];
+                $author     =$row['author'];
+                $author_img =$row['author_image'];
+                $time       =$row['time'];
+                $type       =$row['type'];
+                $is_background  =$row['is_background'];
+                $background_link    =$row['background_link'];
+                $cat_id         =$row['cat_id'];
+
+                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully',
+                'is_background'=>$is_background,'background_link'=>$background_link,
+                'cat_id'=>$cat_id);
+            }
+        }else{
+            $msg[]  =array('status'=>'fail','status_msg'=>'Couldn\'t fetch data from server, reason be that Quote ID not found or Server is Busy!');
+        }
+        echo json_encode($msg);
+	}
 
 
 
@@ -71,8 +126,10 @@ class Api extends My_Controller {
                 $author_img =$row['author_image'];
                 $time       =$row['time'];
                 $type       =$row['type'];
+                $comment_counter    ='3';
 
-                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully');
+                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success',
+                'status_msg'=>'Data fetched successfully','comment_counter'=>$comment_counter);
             }
         }else{
             $msg[]  =array('status'=>'fail','status_msg'=>'Couldn\'t fetch data from server');
@@ -94,8 +151,10 @@ class Api extends My_Controller {
                 $author_img =$row['author_image'];
                 $time       =$row['time'];
                 $type       =$row['type'];
+                $comment_counter    ='3';
 
-                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully');
+                $msg[]      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,
+                'status'=>'success','status_msg'=>'Data fetched successfully','comment_counter'=>$comment_counter);
             }
         }else{
             $msg[]  =array('status'=>'fail','status_msg'=>'Couldn\'t fetch data from server');
@@ -128,13 +187,13 @@ class Api extends My_Controller {
                         $comment_body   =$row['body'];
                         $comment_time   =$row['time'];
 
-                         $data[]      =array('com_id'=>$com_id,'com_author'=>$commenter,'com_body'=>$comment_body,'com_time'=>$comment_time);
+                         $msg[]      =array('com_id'=>$com_id,'com_author'=>$commenter,'com_body'=>$comment_body,'com_time'=>$comment_time);
                     }
                 }else{
                     $msg[]  =array('status'=>'fail','status_msg'=>'Couldn\'t fetch Comment from server');
                 }
 
-                $msg      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully','data'=>$data);
+//                $msg      =array('id'=>$id,'title'=>$title,'desc'=>$desc,'image'=>$image,'author'=>$author,'author_image'=>$author_img,'time'=>$time,'type'=>$type,'status'=>'success','status_msg'=>'Data fetched successfully','data'=>$data);
 
 
             }
@@ -143,4 +202,24 @@ class Api extends My_Controller {
         }
         echo json_encode($msg);
 	}
+
+    public function make_comment(){
+        $msg =array();
+        $fname      =$this->input->post('name');
+        $email      =$this->input->post('email');
+        $comment    =$this->input->post('comment');
+        $forum_id   =$this->input->post('forum_id');
+
+        $data   =array('forum_id'=>$forum_id,'author'=>$fname,'email'=>$email,'body'=>$comment,'time'=>time());
+        $this->db->set($data);
+        $this->db->insert('forum_comment');
+        if($this->db->affected_rows() > 0){
+            $msg['status'] = true;
+
+        }else{
+            $msg['status'] = false;
+        }
+        echo json_encode($msg);
+
+    }
 }
--- application/models/Action.php
+++ application/models/Action.php
@@ -19,6 +19,24 @@ class Action extends My_Model{
         }
         return false;
     }
+
+    public function get_category(){
+        $query  =$this->db->get('category');
+        if($query->num_rows() > 0){
+            return $query->result_array();
+        }
+        return false;
+    }
+
+    public function get_quote_by_cat_id($id){
+        $this->db->where('cat_id',$id);
+        $query  =$this->db->get('quote');
+        if($query->num_rows() > 0){
+            return $query->result_array();
+        }
+        return false;
+    }
+
     public function get_forum(){
         $query  =$this->db->get('forum');
         if($query->num_rows() > 0){
